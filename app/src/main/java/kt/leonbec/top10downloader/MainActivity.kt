09c63d8cc37c1d58fc50ea2b58324a49d47312ae
leonbec <dwfinz@gmail.com>
2018-01-16 20:08:51 +1300

    download rss feed from apple website

--- app/src/main/java/kt/leonbec/top10downloader/MainActivity.kt
+++ app/src/main/java/kt/leonbec/top10downloader/MainActivity.kt
@@ -1,12 +1,39 @@
 package kt.leonbec.top10downloader
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DownloadData().execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+    }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            override fun doInBackground(vararg params: String?): String {
+                val rssFeed = downloadXml(params[0])
+                if (rssFeed.isEmpty())
+                    Log.e(TAG, "Error downloading")
+                return rssFeed
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "Result is $result")
+            }
+
+            private fun downloadXml(url: String?): String {
+                return URL(url).readText()
+            }
+        }
     }
 }
+
